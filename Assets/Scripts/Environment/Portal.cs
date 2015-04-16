e3fe3a2a24e559e8a2e79e13aab1b8b8522644f4
Kirill <kovalewskiy@gmail.com>
2015-04-16 00:47:36 -0700

    start portals logic

---
+++ Assets/Scripts/Environment/Portal.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+	public GameObject otherPortal;
+	public bool rightPortal;
+	
+	private int dropDirection;
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (rightPortal)
+			dropDirection = 1;
+		else
+			dropDirection = -1;
+	
+		if (other.tag == "Player") 
+		{
+			other.transform.position = otherPortal.transform.position + otherPortal.transform.right * dropDirection;
+		}
+		
+	}
+}
