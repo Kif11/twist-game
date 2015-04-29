3ab8edd2876136f456583326587b8a8676ed21da
unknown <jphang@B30RM20.animvfx.academyart.edu>
2015-04-28 19:17:43 -0700

    move ref out of asset

---
+++ References/Scripts/Item.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Item creator
+/// </summary>
+public class Item{
+	public int ID;
+	public string itemName;
+	public string itemDescription;
+	public itemType thisItemType;
+
+	public enum itemType{
+		key,
+		other
+	}
+
+	public Item(int _ID, string _itemName, string _itemDescription, itemType _thisItemType)
+	{
+		ID = _ID;
+		itemName = _itemName;
+		itemDescription = _itemDescription;
+		thisItemType = _thisItemType;
+		
+	}
+
+	public Item()
+	{
+
+	}
+}
---
+++ References/Scripts/Singleton/OtherScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OtherScript : MonoBehaviour {
+
+	private SingletonExample singleton; 
+	// Use this for initialization
+	void Start () {
+		singleton = SingletonExample.instance;
+		singleton.health += 20;			// using health attribute "SET"
+		Debug.Log(singleton.health); 	// using health attribute "GET"  
+		StartCoroutine(singleton.LoadNextScene("OtherScene", 1.5f)); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
